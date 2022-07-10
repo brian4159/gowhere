003296c567013df30e94d431f52490b351367584
brian4159 <88099867+brian4159@users.noreply.github.com>
2022-07-11 00:10:14 +0800

    first commit

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <home-header></home-header>
+</template>
+
+<script>
+import HomeHeader from './components/Header'
+export default {
+    // eslint-disable-next-line vue/multi-word-component-names
+    name:'Home',
+    components:{
+     
+        HomeHeader
+    }
+}
+</script>
+
+<style>
+
+</style>
