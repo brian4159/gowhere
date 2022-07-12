cd4fd04b9fdc26041851a76bf8caf66d15feab61
brian4159 <88099867+brian4159@users.noreply.github.com>
2022-07-12 10:54:02 +0800

    add

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,26 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
 
 <template>
-    <home-header></home-header>
+   <div>
+   <home-header></home-header>
+    <home-swiper></home-swiper>
+    <div>test</div>
+   </div>
+    
 </template>
 
 <script>
 import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name:'Home',
     components:{
      
-        HomeHeader
+        HomeHeader,
+        // eslint-disable-next-line vue/no-unused-components
+        HomeSwiper
+        
     }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption"  >
+     <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" 
+        :src="item.imgUrl" 
+        />
+      </swiper-slide>
+      
+  
+    <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'HomeSwiper',
+    data(){
+        return{
+            swiperOption: {
+              pagination:'.swiper-pagination',
+              loop:true
+        },
+        swiperList:[
+          {
+          id:'0001',
+          imgUrl:"https://img1.qunarzz.com/travel/poi/1504/60/1d8518f48e5283.jpg_r_640x290x70_29c73fbf.jpg"
+        },
+        {id:'0002',
+          imgUrl:"https://img1.qunarzz.com/travel/poi/1504/48/43366084ff7fe1.jpg_r_640x290x70_697366f0.jpg" 
+          }]
+        }
+    },
+    computed: {
+     
+    },
+  
+}
+</script>
+
+<style lang="stylus" scoped>
+.wrapper >>> .swiper-pagination-bullet-active
+        background: #fff !important
+  .wrapper
+    overflow: hidden
+    width:100%
+    height: 0
+    padding-bottom: 31.25%
+    background-color #eee
+ 
+      .swiper-img
+           width: 100%
+      
+  
+  
+    
+</style>
