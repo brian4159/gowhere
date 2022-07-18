926583ee3224575c290a96d6cc72f12090c37a18
brian4159 <88099867+brian4159@users.noreply.github.com>
2022-07-18 21:48:50 +0800

    add icons

--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -1,77 +1,101 @@
 <template>
   <div class="icons">
-    <div class="icon" >
+    <swiper>
+    <swiper-slide v-for="(page,index) of pages" :key="index">
+    <div class="icon" v-for="item of page" :key="item.id" >
       <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
+        <img  class="icon-imgcontent"  :src='item.imgUrl'>
+      </div> 
+       <p class="icon-desc">{{item.desc}}</p>
     </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
+    </swiper-slide>
+    
+    
+    </swiper>
     </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
-    </div>
-    <div class="icon" >
-      <div class="icon-img">
-        <img  class="icon-imgcontent" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" alt="">
-      </div>
-       <p class="icon-desc">热门景点</p>
-    </div>
+     
+    
    
-  </div>
+  
 </template>
 
 <script>
+
 export default {
-    name:'HomeIcons'
+    name:'HomeIcons',
+    data(){
+      return{
+        iconList: [{
+        "id": "0001",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png",
+        "desc": "景点门票"
+      }, {
+        "id": "0002",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1711/df/86cbcfc533330d02.png",
+        "desc": "滑雪季"
+      }, {
+        "id": "0003",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1710/a6/83f636bd75ae6302.png",
+        "desc": "泡温泉"
+      }, {
+        "id": "0004",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/35/2640cab202c41b02.png",
+        "desc": "动植园"
+      }, {
+        "id": "0005",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/d0/e09575e66f4aa402.png",
+        "desc": "游乐园"
+      }, {
+        "id": "0006",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/59/569d3c096e542502.png",
+        "desc": "必游榜单"
+      }, {
+        "id": "0007",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/17/4bd370f3eb1acd02.png",
+        "desc": "演出"
+      }, {
+        "id": "0008",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/7f/b1ea3c8c7fb6db02.png",
+        "desc": "城市观光"
+      }, {
+        "id": "0009",
+        "imgUrl": "http://img1.qunarzz.com/piao/fusion/1611/a9/ffc620dbda9b9c02.png",
+        "desc": "一日游"
+      }],
+      }
+    },
+    computed:{
+        pages (){
+          const pages = []
+          this.iconList.forEach((item,index) => {
+            const page = Math.floor(index / 8)
+            if(!pages[page]){
+              pages[page]=[]
+            }
+            pages[page].push(item)
+          })
+          return pages
+        }
+
+      }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import '~@/assets/styles/varibles.styl';
-    .icons
-        overflow: hidden
+@import '~@/assets/styles/mixins.styl';
+    .icons   >>> .swiper-container
+       
         height :0
         padding-bottom: 50%
-        .icon
+      .icon
             position relative
             overflow: hidden
             float: left
-            width 25%
+            width 25% 
             padding-bottom: 25%
             height 0
-            .icon-img
+          .icon-img
               position absolute
               top: 0
               left: 0
@@ -79,11 +103,11 @@ export default {
               bottom: .44rem
               box-sizing:border-box
               padding 0.1rem 
-              .icon-imgcontent
+            .icon-imgcontent
                 height: 100%
                 display:block
                 margin:0 auto
-            .icon-desc
+          .icon-desc
               position absolute
               bottom 0
               left 0
@@ -92,5 +116,7 @@ export default {
               line-height: .44rem
               color:$darkTextColor
               text-align center
+              ellipsis()
+
 
 </style>
