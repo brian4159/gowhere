12f092b8247f0a7dcfacfe746c762c1cb6f968bb
brian4159 <88099867+brian4159@users.noreply.github.com>
2022-07-20 18:45:41 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,16 +1,22 @@
+<!-- eslint-disable vue/no-multiple-template-root -->
 <template>
+<div>
   <city-header></city-header>
+  <city-search></city-search>
+</div>
 </template>
 
 <script>
 
 // eslint-disable-next-line no-unused-vars
 import CityHeader from './components/Header.vue'
+import CitySearch from './components/Search.vue'
 export default {
     // eslint-disable-next-line vue/multi-word-component-names
     name:'City',
     components:{
-        CityHeader
+        CityHeader,
+        CitySearch
     }
 }
 </script>
